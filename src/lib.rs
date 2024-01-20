8bf9af5bd2e70155bab886657f214b1b5aa893cd
drewmcarthur <drewmcarthur1@gmail.com>
2024-01-20 14:27:52 -0700

    create works

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 use worker::*;
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct Target {
+    url: String,
+    password: Option<String>,
+}
+
+const TRACKED_URLS_STORE: &str = "QRSTATS_TRACKED_URLS_BY_ID";
+
 #[event(fetch)]
-async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
+async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     Router::new()
         .get("/", index)
-        .post("/create", create)
+        .post_async("/create", create)
         .get("/redirect/{id}", redirect)
         .get("/stats", stats_login)
         .post("/stats/{id}", stats)
@@ -12,22 +22,69 @@ async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
         .await
 }
 
-fn index(req: Request, ctx: RouteContext<()>) -> Result<Response> {
-    Response::ok("Hello, Index!")
+fn index(_req: Request, _ctx: RouteContext<()>) -> Result<Response> {
+    serve_html(include_str!("public/index.html"))
 }
 
-fn create(req: Request, ctx: RouteContext<()>) -> Result<Response> {
-    Response::ok("Hello, Created World!")
+async fn create(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let target = get_target(req).await?;
+    let new_id = Uuid::new_v4().to_string();
+
+    let kv = ctx.kv(TRACKED_URLS_STORE)?;
+    kv.put(&new_id, target.clone())?.execute().await?;
+
+    Response::ok(format!(
+        "Success! New ID for URL(\"{}\") is: {}",
+        target.url, new_id
+    ))
 }
 
-fn redirect(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+async fn get_target(req: Request) -> Result<Target> {
+    let mut req = req;
+    let data: FormData = req.form_data().await?;
+
+    let missing_url_error = Error::Internal("No URL in /create request body".into());
+    let bad_url_format_error =
+        Error::Internal("URL should be String, not File in /create request body".into());
+
+    let new_url = match data.get("url").ok_or(missing_url_error)? {
+        FormEntry::Field(s) => s,
+        FormEntry::File(_) => return Err(bad_url_format_error),
+    };
+
+    let pw = match data.get("password") {
+        Some(FormEntry::Field(s)) => Some(s),
+        _ => None,
+    };
+
+    Ok(Target {
+        url: new_url.clone(),
+        password: pw.map(|pw| pw.to_string()),
+    })
+}
+
+fn redirect(_req: Request, _ctx: RouteContext<()>) -> Result<Response> {
     Response::ok("Hello, Redirected World!")
 }
 
-fn stats_login(req: Request, ctx: RouteContext<()>) -> Result<Response> {
-    Response::ok("Hello, Stats Login World!")
+fn stats_login(_req: Request, _ctx: RouteContext<()>) -> Result<Response> {
+    let mut res = Response::ok(include_str!("public/stats.html"));
+    set_html_content_type(&mut res);
+    res
 }
 
-fn stats(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+fn stats(_req: Request, _ctx: RouteContext<()>) -> Result<Response> {
     Response::ok("Hello, Stats World!")
+}
+
+fn set_html_content_type(res: &mut Result<Response>) {
+    res.as_mut()
+        .map(|res| res.headers_mut().set("Content-Type", "text/html").unwrap())
+        .unwrap();
+}
+
+fn serve_html(html: &str) -> Result<Response> {
+    let mut res = Response::ok(html);
+    set_html_content_type(&mut res);
+    res
 }
