77d73752a41878a28a484fef1f94eccbf5c53614
drewmcarthur <drewmcarthur1@gmail.com>
2024-01-20 22:03:04 -0700

    universal formatting

--- src/views.rs
+++ src/views.rs
@@ -3,21 +3,29 @@ use worker::{Response, Result};
 use crate::{util::serve_html, Stats, Target};
 
 pub(crate) fn stats_view(stats: Stats) -> Result<Response> {
-    Response::ok(stats.to_string())
+    let body = format!(
+        "<h1>Stats</h1>
+        <h2>ID</h2>
+        <p>{}</p>
+        <h2>Count</h2>
+        <p>{}</p>",
+        stats.id, stats.count
+    );
+    let html = format!(include_str!("./public/layout.html"), body);
+    serve_html(html.as_str())
 }
 
 pub(crate) fn create_success(target: Target, id: String) -> Result<Response> {
     let redirect_url = format!("/redirect/{}", id);
     let stats_url = format!("/stats/{}", id);
 
-    serve_html(
-        format!(
+    let body = format!(
             "<h1>Success!</h1>
             <p>New ID for URL(\"{}\") is: {}</p>
             <p><a href=\"{}\">Redirect URL</a> (copy this into your QR generator</p>
             <p><a href=\"{}\">Stats URL</a> (visit here, and enter your ID and password to view your stats)</p>",
             target.url, id, redirect_url, stats_url
-        )
-        .as_str(),
-    )
+        );
+    let html = format!(include_str!("./public/layout.html"), body);
+    serve_html(html.as_str())
 }
